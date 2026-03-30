9dc926e004bea6b2f32b24e5396325a214469f2e
Valerie <valeriechia07@gmail.com>
2026-03-30 16:20:32 +0800

    ahhh

--- server/server.js
+++ server/server.js
@@ -45,8 +45,10 @@ You are NOT a counselor. You are just a normal person chatting.
 });
 const express = require('express');
 const app = express();
-app.use(express.json());
 app.use(express.static(path.join(__dirname, '..')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
 
 
 app.post("/ai-chat", async (req, res) => {      
