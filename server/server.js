56250ea1a01d56639a3a625f45a9712494e32f41
Valerie <valeriechia07@gmail.com>
2026-03-30 16:10:47 +0800

    rushing against time

--- server/server.js
+++ server/server.js
@@ -45,8 +45,8 @@ You are NOT a counselor. You are just a normal person chatting.
 });
 const express = require('express');
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Backend is running!");
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 app.use(express.static(path.join(__dirname, '..')));
 app.use(express.json());
